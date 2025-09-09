a081e670ed2055ad6553a372a6bf8c2dd8183950
professordiogodev <diogo@dionamite.com>
2025-09-09 13:41:48 +0100

    adding slow version

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const app = require("express")()
+require("dotenv").config()
+
 const desiredPath = process.env.DESIRED_PATH || "/"
 const port = process.env.PORT || 80
 const number = process.env.NUMBER || 0
 
 app.get(desiredPath, (req, res) => {
-    res.send(`<h1>Hello from ${desiredPath} number ${number}!</h1>`)
+    setTimeout(() => {
+        res.send(`<h1>(Slow) Hello from ${desiredPath} number ${number}!</h1>`)
+    }, 6000) // 6000 ms = 6 seconds
 })
 
 app.get("/healthcheck", (req, res) => {
-    res.status(200).send("It works!")
+    setTimeout(() => {
+        res.status(200).send("It slowly works!")
+    }, 6000) // 6000 ms = 6 seconds
 })
 
 app.listen(port, () => {
