b46e474bebc1fe644fd097010c011c8ed7bca1d4
professordiogodev <diogo@dionamite.com>
2025-11-19 16:08:03 +0000

    slow message

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const port = process.env.PORT || 8080
 const number = process.env.NUMBER || 0
 
 app.get(desiredPath, (req, res) => {
-    setTimeout(() => {
-        res.send(`<h1>(Slow) Hello from ${desiredPath} number ${number}!</h1>`)
-    }, 6000) // 6000 ms = 6 seconds
+  res.send(`<h1>Hello from ${desiredPath} number ${number}!</h1>`)
 })
 
 app.get("/healthcheck", (req, res) => {
-    setTimeout(() => {
-        res.status(200).send("It slowly works!")
-    }, 6000) // 6000 ms = 6 seconds
+    res.status(200).send("It works!")
 })
 
 app.listen(port, () => {
-    console.log(`🚀 Server ${number} listening on ${port} at path ${desiredPath}`)
+  console.log(`🚀 Server ${number} listening on ${port} at path ${desiredPath}`)
 })
